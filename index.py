f2a1f377a092b8f4cc8e042ea9fd7e43f7a8a192
physicscoaching1 <39644583+physicscoaching1@users.noreply.github.com>
2018-05-28 16:14:23 +0530

    Updates1
    Better GUI. Delete Clutters

--- index.py
+++ index.py
@@ -3,33 +3,40 @@ import dash_html_components as html
 from dash.dependencies import Input, Output
 
 from app import app, server
-from apps import app1_charts1, app2_charts2
+from apps import app1_charts1, app2_charts2, home, macro
 
 #%%
+app.css.append_css({
+    'external_url': 'https://codepen.io/chriddyp/pen/bWLwgP.css'
+    })
 
-tabstyle = {'background-color': 'White', 'border-bottom': '1pt solid Blue'}
-websitenamestyle = {'color': 'Black', 'background-color': 'White', 'font-size': '170%', 'border-bottom': '1pt solid Blue' }
-#buttonstyle = {'textAlign': 'center', 'font-size': '100%', 'background-color': 'DodgerBlue' , 'border': '2pt solid Black'}
-#buttonstyle = {'textAlign': 'center', 'background-color': 'DodgerBlue'}
-buttonstyle = {'textAlign': 'center', 'background-color': 'White', 'border': 'none', 'font-size': '110%'}
-dcclinkstyle = {'color': 'Black', 'text-decoration':'none'}
-#dcclinkstyle = {'color': 'White', 'text-decoration':'none'}
+#buttonstyle = {'textAlign': 'center', 'background-color': '#0275d8', 'border': 'none', 'font-size': '110%'}
+#dcclinkstyle = {'color': 'Black', 'text-decoration':'none'}
+buttonstyle = {'textAlign': 'right'}
+Headerstyle = { 'border-bottom': '1pt solid Black'}
+WebsiteNamestyle = {'font-family': 'Times New Roman', 'font-size': '150%'}
+
 
 #%%
 
 
 app.layout = html.Div([
     dcc.Location(id='url', refresh=False),
-    html.Div(children=[html.B('GlobalInvestor')
-                      ], style = websitenamestyle ),
     html.Div([
-        html.Button(dcc.Link('   Home   ', href='/', style = dcclinkstyle ), style = buttonstyle),
-        html.Button(dcc.Link('   Fundamental Charts   ', href='/FundamentalCharts', style = dcclinkstyle ), style = buttonstyle),
-        html.Button(dcc.Link('   Backtest   ', href='/Backtest', style = dcclinkstyle ), style = buttonstyle),
+            html.Div(children=[html.Div('Global Investor'),
+                       #html.H4('GlobalInvestor', style = websitenamestyle)
+                      ], className="six columns", style = WebsiteNamestyle),
+            html.Div([
+                html.Div(dcc.Link('   Home   ', href='/',  className="link"), className = "button"),
+                html.Div(dcc.Link('   Stocks   ', href='/FundamentalCharts',className="link"),  className="button"),
+                html.Div(dcc.Link('   Macro   ', href='/Macro',className="link"),  className="button"),
+                html.Div(dcc.Link('   Backtest   ', href='/Backtest', className="link"),  className="button"),
 
-    ], style = tabstyle),
-    #html.Br(),
-    html.Div(id='page-content')
+                    ], className="six columns", style = buttonstyle),
+            ], style = Headerstyle, className="row"),
+
+                        #html.Br(),
+    html.Div(id='page-content', className="row")
 ])
 
 
@@ -37,11 +44,13 @@ app.layout = html.Div([
               [Input('url', 'pathname')])
 def display_page(pathname):
     if pathname == '/':
-         return "Home"
+         return home.layout
     if pathname == '/FundamentalCharts':
          return app1_charts1.layout
     elif pathname == '/Backtest':
          return app2_charts2.layout
+    elif pathname == '/Macro':
+         return macro.layout
     else:
         return 'Incorrect URL. Please enter correct URL or go to home'
 
